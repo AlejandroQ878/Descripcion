23d503f7bec22d578933e2ef28d19f68a2b98d59
AlejandroQ878 <alejandroquiroga2006@gmail.com>
2025-02-21 19:49:24 -0500

    Descripcion de mas comandos

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("git status");
-
+        System.out.println("git add .");
+        System.out.println("git commit -m");
+        System.out.println("git push origin main");
+        System.out.println("git switch -c");
+        System.out.println("git branch");
+        System.out.println("git switch main");
+        System.out.println("git push origin main");
+        System.out.println("git pull origin main");
         }
     }
